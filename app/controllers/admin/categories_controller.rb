7f3b7054f6c8fa01779565a0244a6e7f158b8433
ArenRitz <aren.dolbakian@gmail.com>
2022-08-25 13:18:47 -0400

    Define views in controlelr and  initialize required instanced variable

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,13 +3,27 @@ class Admin::CategoriesController < ApplicationController
   pswd = ENV["HTTP_B_AUTH_PSWD"]
   http_basic_authenticate_with name: user, password: pswd
   def create
+    
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: "Category created!"
+    else
+      render :new
+    end
   end
 
   def new
+    @category = Category.new
   end
 
   def index
+    @categories = Category.all
   end
 
-  
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
